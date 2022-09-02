71ae46405d791791bd4f708bbae6b377a3299c56
hevedrios <exxed.ridel@gmail.com>
2022-09-02 18:37:36 -0500

    Added react router template

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/global.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h4>Navbar</h4>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="about">About</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const root = ReactDOM.createRoot(
+   document.getElementById("root")
+);
+
+root.render(
+   <React.StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
