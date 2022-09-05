ac0efec2efde9b189590480dc9f2b19c0baf9eb6
hevedrios <exxed.ridel@gmail.com>
2022-09-05 14:27:16 -0500

    Fixed: Redirect routing over Netlify and Navbar and styles added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,40 @@
 import React from "react";
-import "../styles/global.scss";
+import "../styles/Navbar.scss";
 import { Link } from "react-router-dom";
+import menu from "../assets/icons/icon_menu.svg";
+import logo from "../assets/images/logo_bajo_musica.png";
 
 const App = () => {
   return (
-    <div>
-      <h4>Navbar</h4>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </div>
+    <nav className="navbar">
+      <img src={menu} alt="menu" className="menu" />
+
+      <div className="navbar-left">
+        <Link to="/">
+          <img src={logo} alt="logo" className="nav-logo" />
+        </Link>
+
+        <ul>
+          <li>
+            <Link to="/vlog">Vlog</Link>
+          </li>
+          <li>
+            <Link to="/news">Noticias</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contacto</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <ul>
+          <li className="navbar-email">hevedrios@bajolamusica.com</li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import "./styles/global.scss";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
