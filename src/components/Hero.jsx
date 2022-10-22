8d990fe6b808983c054bcca64b6f93f97522338e
hevedrios <exxed.ridel@gmail.com>
2022-10-21 22:36:04 -0500

    Added: npm axios (working), and @babel/plugin-transform-runtime (not working)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Hero.scss";
-import heroData from "../data/heroData.js";
+// import heroData from "../data/heroData.js";
+import axios from "axios";
 
 const Hero = () => {
   const [heroImage, setHeroImage] = React.useState("");
 
-  // Call to unesplash api for random images of a topic
-  // React.useEffect(() => {
-  //   fetch(
-  //     "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=concert"
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setHeroImage(data.urls.regular);
-  //     });
+  /* Call to unesplash api for random images of a topic */
+  useEffect(() => {
+    axios.get("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=concert")
+      .then((res) => setHeroImage(res.data.urls.regular))
+      .catch((error) => {console.log(error)});
+  }, []);
+
+  /* Same call with @babel/plugin-transform-runtime, not working */
+  // useEffect(async () => {
+  //   const response = await axios("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=concert");
+  //   setHeroImage(response.data);
   // }, []);
 
-  React.useEffect(() => {
-    const randomNumber = Math.floor(Math.random() * heroData.length);
-    setHeroImage(heroData[randomNumber].imageUrl);
-  }, []);
+  /* random images from local array */
+  // React.useEffect(() => {
+  //   const randomNumber = Math.floor(Math.random() * heroData.length);
+  //   setHeroImage(heroData[randomNumber].imageUrl);
+  // }, []);
 
   return (
     <div className="hero-container">
