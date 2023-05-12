519a4d555130b1dc6568af66fa0b3f2fddd5022a
exxedridel <exxed.ridel@gmail.com>
2023-05-12 12:46:57 -0600

    ADD new event entry

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -8,7 +8,7 @@ export default [
       "https://images.unsplash.com/photo-1608829206962-f9e9272b1f7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     link: "https://www.facebook.com/events/587673002962680/?acontext=%7B%22event_action_history%22%3A[%7B%22surface%22%3A%22search%22%7D%2C%7B%22mechanism%22%3A%22search_results%22%2C%22surface%22%3A%22search%22%7D]%2C%22ref_notif_type%22%3Anull%7D",
     date: {
-      day: 11,
+      day: 12,
       month: 5,
       fullYear: 2023,
     },
